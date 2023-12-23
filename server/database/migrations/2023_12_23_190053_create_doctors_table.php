0fa2bbf748ecbcf05e45414b51abde5e737c21a8
unknown <mohammad.fakih685@gmail.com>
2023-12-23 21:15:46 +0200

    users migrations implemented

---
+++ server/database/migrations/2023_12_23_190053_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('last_name');
+            $table->string('specialty');
+            $table->integer('age');
+            $table->integer('phone_number');
+            $table->integer('license_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
