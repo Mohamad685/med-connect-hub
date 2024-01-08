9a8c4afcb74fd98c31cf1e22a001c7200f87133e
unknown <mohammad.fakih685@gmail.com>
2024-01-08 20:12:07 +0200

    Frontend<> added navbar options

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./NavBar/NavBar";
-
+import {BrowserRouter} from 'react-router-dom';
 function App() {
 	return (
+    <BrowserRouter>
 			<NavBar />
+    </BrowserRouter>
 	);
 }
 
--- Frontend/src/NavBar/NavBar.jsx
+++ Frontend/src/NavBar/NavBar.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import {Link} from "react-router-dom";
 import "../NavBar/NavBar.css";
 
 function NavBar(){
+    const scrollTo = (sectionId) => {
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+      };
     return(
         <>
         <div className="navbar">
             <img src='../../src/assets/pics/logo.png' className="navbar-pic" alt="logo"></img>
+            <ul className="navbar-list">
+                <li className="navbar-home">
+                <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="navbar-about" onClick={()=> scrollTo('aboutus')}>About Us</li>
+                <li className="navbar-services"onClick={()=> scrollTo('services')}>Services</li>
+                <li className="navbar-contact" onClick={()=> scrollTo('Contact')}>Contact Us</li>
+                <li className="login">Login</li>
+            </ul>
         </div>
+        
         </>
     );
 }
