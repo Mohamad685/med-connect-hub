c535a417c516b929b8520be2ffb539d0262eba23
unknown <mohammad.fakih685@gmail.com>
2024-01-09 11:22:55 +0200

    Frontend<>Added services title

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -14,10 +14,18 @@ function HomePage() {
 				<span className="title-start">
 					What We <span className="title-start title-end">Do</span>
 				</span>
-                <span className="mission-text"> To streamline healthcare management by connecting patients, doctors, and insurance providers on a single, secure platform. 
-                Our mission is to enhance communication and collaboration between all parties, improving access to care, simplifying healthcare processes, and promoting informed health decisions. 
-                We're committed to delivering a user-friendly experience that facilitates real-time conversations, seamless information exchange, and comprehensive support every step of the way.
-                </span>
+				<span className="mission-text">
+					{" "}
+					To streamline healthcare management by connecting patients, doctors,
+					and insurance providers on a single, secure platform. Our mission is
+					to enhance communication and collaboration between all parties,
+					improving access to care, simplifying healthcare processes, and
+					promoting informed health decisions. We're committed to delivering a
+					user-friendly experience that facilitates real-time conversations,
+					seamless information exchange, and comprehensive support every step of
+					the way.
+				</span>
+                <span>Our<span>Services</span></span>
 			</div>
 		</>
 	);
