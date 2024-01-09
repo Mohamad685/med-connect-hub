864ee484a9b6e924e1597835872843c1bde0abae
unknown <mohammad.fakih685@gmail.com>
2024-01-09 23:54:10 +0200

    Frontend<> Changed swiper bullet color

--- Frontend/src/Components/Carousal/InfoCarousal.jsx
+++ Frontend/src/Components/Carousal/InfoCarousal.jsx
@@ -1,14 +1,8 @@
 import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
 import './InfoCarousal.css';
-
-// import required modules
 import { Pagination } from 'swiper/modules';
 
 export default function Carousel() {
