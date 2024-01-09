c2880196cf6a94c8741b447b4cb963e1015060c9
unknown <mohammad.fakih685@gmail.com>
2024-01-09 11:01:07 +0200

    Frontend<> Added mission title

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -7,6 +7,7 @@ function HomePage(){
 
         <div className='hero-section'>
         <img src="../../src/assets/pics/hero.png" alt="hero" className='hero-pic'></img>
+        <span className='title-start'>What We <span className='title-start title-end'>Do</span></span>
         </div>
         </>
     )
