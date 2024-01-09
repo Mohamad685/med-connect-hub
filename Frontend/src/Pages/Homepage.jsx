68ff13b6a39bf9d66f48ab2948221acec7295f70
unknown <mohammad.fakih685@gmail.com>
2024-01-09 11:03:14 +0200

    Frontend<> Added div to home page

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -4,9 +4,10 @@ import './HomePage.css'
 function HomePage(){
     return(
         <>
-
+        <div>
         <div className='hero-section'>
         <img src="../../src/assets/pics/hero.png" alt="hero" className='hero-pic'></img>
+        </div>        
         <span className='title-start'>What We <span className='title-start title-end'>Do</span></span>
         </div>
         </>
