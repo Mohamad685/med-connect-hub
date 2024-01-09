6fbb375903cda71384d62837be426960ca3b631f
unknown <mohammad.fakih685@gmail.com>
2024-01-09 11:11:55 +0200

    Frontend<>Added and designed mission text

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import './HomePage.css'
+import React from "react";
+import "./HomePage.css";
 
-function HomePage(){
-    return(
-        <>
-        <div className='home-page'>
-        <div className='hero-section'>
-        <img src="../../src/assets/pics/hero.png" alt="hero" className='hero-pic'></img>
-        </div>        
-        <span className='title-start'>What We <span className='title-start title-end'>Do</span></span>
-        </div>
-        </>
-    )
+function HomePage() {
+	return (
+		<>
+			<div className="home-page">
+				<div className="hero-section">
+					<img
+						src="../../src/assets/pics/hero.png"
+						alt="hero"
+						className="hero-pic"></img>
+				</div>
+				<span className="title-start">
+					What We <span className="title-start title-end">Do</span>
+				</span>
+                <span className="mission-text"> To streamline healthcare management by connecting patients, doctors, and insurance providers on a single, secure platform. 
+                Our mission is to enhance communication and collaboration between all parties, improving access to care, simplifying healthcare processes, and promoting informed health decisions. 
+                We're committed to delivering a user-friendly experience that facilitates real-time conversations, seamless information exchange, and comprehensive support every step of the way.
+                </span>
+			</div>
+		</>
+	);
 }
 
 export default HomePage;
