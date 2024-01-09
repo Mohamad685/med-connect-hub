4d69db4af34a17c670e637a03ce3b5430dabb3f9
unknown <mohammad.fakih685@gmail.com>
2024-01-10 00:10:15 +0200

    Frontend<>Updated carousal direction

--- Frontend/src/Components/Carousal/InfoCarousal.jsx
+++ Frontend/src/Components/Carousal/InfoCarousal.jsx
@@ -1,35 +1,41 @@
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import './InfoCarousal.css';
-import { Pagination, Autoplay } from 'swiper/modules';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./InfoCarousal.css";
+import { Pagination, Autoplay } from "swiper/modules";
 
 export default function Carousel() {
-  return (
-    <>
-      <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        autoplay={{
-          delay: 2500,  // Set the delay in milliseconds
-          disableOnInteraction: false,  // Continue autoplay after user interaction
-        }}
-        speed={2500}
-        modules={[Pagination, Autoplay]}  // Add the Autoplay module here
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
-  );
+	const [reverse, setReverse] = useState(false);
+
+	const handleSwiperEnd = (swiper) => {
+		if (swiper.isEnd) {
+			setReverse(!reverse); // Toggle the direction
+		}
+	};
+	return (
+		<>
+			<Swiper
+				pagination={{ dynamicBullets: true }}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+					reverseDirection: reverse,
+				}}
+				onReachEnd={handleSwiperEnd}
+				modules={[Pagination, Autoplay]}
+				className="mySwiper">
+                    
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</>
+	);
 }
