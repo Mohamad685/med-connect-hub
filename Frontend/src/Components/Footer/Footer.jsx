b0049092f37625f7c1c9d0af0874a5ab4393250a
unknown <mohammad.fakih685@gmail.com>
2024-01-10 21:43:10 +0200

    Frontend<>Added copyright text

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
 function Footer() {
 	return (
 		<>
 			<div className="footer">
-				<div className="footer-in">
-
-				</div>
-
-            </div>
+				<div className="footer-in"></div>
+			</div>
 			<div className="copyright">
-
+				<p>© 2024 Med-Connect Hub. All rights reserved.</p>
 			</div>
 		</>
 	);
