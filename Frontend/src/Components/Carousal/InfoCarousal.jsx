1dfe41ed60659a37051f70098160786101ee3049
unknown <mohammad.fakih685@gmail.com>
2024-01-10 10:45:22 +0200

    Frontend<>Added Diabetes Info

--- Frontend/src/Components/Carousal/InfoCarousal.jsx
+++ Frontend/src/Components/Carousal/InfoCarousal.jsx
@@ -23,28 +23,64 @@ export default function Carousel() {
 					reverseDirection: reverse,
 				}}
 				onReachEnd={handleSwiperEnd}
+				speed={2500}
 				modules={[Pagination, Autoplay]}
 				className="mySwiper">
 				<SwiperSlide>
 					{" "}
-						<span className="info-title">
-							New Information
-							<p className="info-text">
-								Smoking causes significant DNA damage through mutations,
-								oxidative stress, and epi-genetic changes, leading to disrupted
-								cell functions and an increased risk of cancers and other
-								diseases. These molecular alterations highlight the severe
-								health impacts of smoking.
-							</p>
-						</span>
-						<img
-							src="../../src/assets/pics/DNA.png"
-							alt="DNA pic"
-							className="DNA-info"
-						/>
+					<span className="info-title">
+						New Information
+						<p className="info-text">
+							Stress affects diabetes by altering blood sugar levels and
+							disrupting self-care routines. Managing it through exercise,
+							mindfulness, a balanced diet, good sleep, support networks, and
+							professional help is crucial for effective diabetes control.
+							Regular monitoring and adjusting diabetes management during stress
+							is also essential.
+						</p>
+					</span>
+					<img
+						src="../../src/assets/pics/DNA.png"
+						alt="DNA pic"
+						className="DNA-info"
+					/>
 				</SwiperSlide>
-				
-				
+				<SwiperSlide>
+					{" "}
+					<span className="info-title">
+						New Information
+						<p className="info-text">
+							Smoking causes significant DNA damage through mutations, oxidative
+							stress, and epi-genetic changes, leading to disrupted cell
+							functions and an increased risk of cancers and other diseases.
+							These molecular alterations highlight the severe health impacts of
+							smoking.
+						</p>
+					</span>
+					<img
+						src="../../src/assets/pics/DNA.png"
+						alt="DNA pic"
+						className="DNA-info"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					{" "}
+					<span className="info-title">
+						New Information
+						<p className="info-text">
+							Smoking causes significant DNA damage through mutations, oxidative
+							stress, and epi-genetic changes, leading to disrupted cell
+							functions and an increased risk of cancers and other diseases.
+							These molecular alterations highlight the severe health impacts of
+							smoking.
+						</p>
+					</span>
+					<img
+						src="../../src/assets/pics/DNA.png"
+						alt="DNA pic"
+						className="DNA-info"
+					/>
+				</SwiperSlide>
 			</Swiper>
 		</>
 	);
