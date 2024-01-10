44ade1da7e67636de3d1307e724e04a7a57c6583
unknown <mohammad.fakih685@gmail.com>
2024-01-10 22:03:18 +0200

    Frontend<>Added contact icons div

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -5,7 +5,13 @@ function Footer() {
 	return (
 		<>
 			<div className="footer">
-				<div className="footer-in"></div>
+				<div className="footer-in">
+					<div className="contact-icons">
+						<img src="" alt="phone"/>
+						<img src="" alt="location"/>
+						<img src="" alt="mail"/>
+					</div>
+				</div>
 			</div>
 			<div className="copyright">
 				<p>© 2024 Med-Hub. All rights reserved.</p>
