f3e9dde111c2efd1412691b301ee65fe058258c9
unknown <mohammad.fakih685@gmail.com>
2024-01-12 01:11:52 +0200

    Frontend<>Added login button to auth

--- Frontend/src/Components/Auth/Auth.jsx
+++ Frontend/src/Components/Auth/Auth.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import "./Auth.css";
 import InputForm from "../Input/Input";
+import Button from "../Button/Button";
 
 function Login() {
 	return (
 		<>
 			<div className="login-form">
-				
-                <h1>Log In To Your Account:</h1>
-                <InputForm
+				<h1>Log In To Your Account:</h1>
+				<InputForm
 					type="text"
 					width={"25rem"}
 					length={"2rem"}
 					placeholder={"Enter your email:"}
 					className="email-input"
 				/>
-                <InputForm
+				<InputForm
 					type="text"
 					width={"25rem"}
 					length={"2rem"}
 					placeholder={"Enter your password:"}
 					className="password-input"
 				/>
-				
+				<Button
+					background={""}
+					color={"white"}
+					width={"15rem"}
+					height={"3rem"}
+					className="login-button">
+					Submit
+				</Button>
 			</div>
 		</>
 	);
