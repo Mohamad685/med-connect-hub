68105ffa9b5db26908218a734d28058c9dc7dafb
unknown <mohammad.fakih685@gmail.com>
2024-01-12 01:09:16 +0200

    Frontend<>Added button component

---
+++ Frontend/src/Components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Button({background, color, width, length}){
+    const buttonStyle={
+        backgroundColor: background,
+        color: color,
+        width:width,
+        height:length,
+    }
+    return(
+        <>
+        <button style={buttonStyle}>Button</button>
+        </>
+    )
+}
+
+export default Button;
