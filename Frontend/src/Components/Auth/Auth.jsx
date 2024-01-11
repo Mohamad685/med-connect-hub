60c1065b79fd8c7d5d061e54ccb8f6d50ac5e7ab
unknown <mohammad.fakih685@gmail.com>
2024-01-12 01:00:38 +0200

    Frontend<>Added email input box

--- Frontend/src/Components/Auth/Auth.jsx
+++ Frontend/src/Components/Auth/Auth.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./Auth.css";
+import InputForm from "../Input/Input";
 
-function Login(){
-    return(
-        <>
-        <div className="login-form">
+function Login() {
+	return (
+		<>
+			<div className="login-form">
+				<InputForm
+					type="text"
+					width={"25rem"}
+					length={"2rem"}
+					placeholder={"Enter your email"}
+					className="email-input"
+				/>
 
-        </div>
-        </>
-
-    );
+				
+			</div>
+		</>
+	);
 }
 
 export default Login;
