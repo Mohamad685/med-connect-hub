8f98fb83c2a84fb9c4093ccb9f931ee28fce24f6
unknown <mohammad.fakih685@gmail.com>
2024-01-12 12:48:28 +0200

    Frontend<>Added and designed the patient registration title page

--- Frontend/src/Pages/PatientRegister/PatientRegister.jsx
+++ Frontend/src/Pages/PatientRegister/PatientRegister.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import OptionsBox from "../../Components/Options/Options";
 import "./PatientRegister.css";
-function PatientRegister(){
-    return(
-        <div className="patient-reg-page">
-            <OptionsBox margin={"4rem 2rem 2rem 2rem"} className="options-style"/>
-        </div>
-    );
+
+function PatientRegister() {
+	return (
+		<div className="patient-reg-page">
+			<OptionsBox
+				margin={"4rem 2rem 2rem 2rem"}
+				className="options-style"
+			/>
+            <p>Create Patient Profile</p>
+		</div>
+	);
 }
 
 export default PatientRegister;
