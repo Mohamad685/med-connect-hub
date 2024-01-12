3db08ee52f3bacdb95500ea7cdfe81b909881aa3
unknown <mohammad.fakih685@gmail.com>
2024-01-12 11:18:22 +0200

    Frontend<>Added options to options box

--- Frontend/src/Components/Options/Options.jsx
+++ Frontend/src/Components/Options/Options.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function OptionsBox(){
-    return(<></>)
+function OptionsBox() {
+	return (
+    
+    <>
+    <div className="options-box">
+        <a href="">Create Patient Profile</a>
+        <a href="">Patients List</a>
+        <a href="">Live Chat</a>
+        <a href="">Send Email</a>
+        <a href="">Notifications</a>
+    </div>
+    </>);
 }
 
 export default OptionsBox;
