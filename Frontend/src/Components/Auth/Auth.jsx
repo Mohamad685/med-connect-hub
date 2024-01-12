8c1521262a0002c234a4dad56dcf45c8329ad92f
unknown <mohammad.fakih685@gmail.com>
2024-01-12 02:04:11 +0200

    Frontend<>Deleted un-necessary div in auth

--- Frontend/src/Components/Auth/Auth.jsx
+++ Frontend/src/Components/Auth/Auth.jsx
@@ -6,8 +6,10 @@ import Button from "../Button/Button";
 function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+
 	const handleEmailChange = (e) => setEmail(e.target.value);
 	const handlePasswordChange = (e) => setPassword(e.target.value);
+
 	const handleSubmit = () => {
 		e.preventDefault();
 		setEmail("");
@@ -15,40 +17,38 @@ function Login() {
 	};
 	return (
 		<>
-			<div className="login-form">
-				<form
-					onSubmit={handleSubmit}
-					className="login-form">
-					<h1>Log In To Your Account:</h1>
+			<form
+				onSubmit={handleSubmit}
+				className="login-form">
+				<h1>Log In To Your Account:</h1>
 
-					<InputForm
-						type="email"
-						width={"25rem"}
-						length={"2rem"}
-						placeholder={"Enter your email:"}
-						className="email-input"
-						value="email"
-						onChange={handleEmailChange}
-					/>
-					<InputForm
-						type="password"
-						width={"25rem"}
-						length={"2rem"}
-						placeholder={"Enter your password:"}
-						className="password-input"
-						value="password"
-						onChange={handlePasswordChange}
-					/>
-					<Button
-						background={"#7bb3df"}
-						color={"white"}
-						width={"15rem"}
-						height={"2rem"}
-						className="login-button">
-						Submit
-					</Button>
-				</form>
-			</div>
+				<InputForm
+					type="email"
+					width={"25rem"}
+					length={"2rem"}
+					placeholder={"Enter your email:"}
+					className="email-input"
+					value={email}
+					onChange={handleEmailChange}
+				/>
+				<InputForm
+					type="password"
+					width={"25rem"}
+					length={"2rem"}
+					placeholder={"Enter your password:"}
+					className="password-input"
+					value={password}
+					onChange={handlePasswordChange}
+				/>
+				<Button
+					background={"#7bb3df"}
+					color={"white"}
+					width={"15rem"}
+					height={"2rem"}
+					className="login-button">
+					Submit
+				</Button>
+			</form>
 		</>
 	);
 }
