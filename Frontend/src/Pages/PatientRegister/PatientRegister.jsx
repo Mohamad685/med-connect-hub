26461c2bc9c3ce38d6ff906afbb30fa096adacdd
unknown <mohammad.fakih685@gmail.com>
2024-01-13 01:15:44 +0200

    Frontend<>Designed the patient register section 1

--- Frontend/src/Pages/PatientRegister/PatientRegister.jsx
+++ Frontend/src/Pages/PatientRegister/PatientRegister.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import OptionsBox from "../../Components/Options/Options";
 import "./PatientRegister.css";
 import ProfilePic from "../../Components/ProfilePic/ProfilePic";
+import InputForm from "../../Components/Input/Input";
 
 function PatientRegister() {
 	return (
@@ -13,9 +14,45 @@ function PatientRegister() {
 
 			<form className="patient-reg-form">
 				<p className="patient-reg-title">Create Patient Profile</p>
-
-				<ProfilePic src={""} 
-				className="patient-reg-pic"/>
+				<div className="patient-reg-section1">
+					<ProfilePic
+						src={""}
+						className="patient-reg-pic"
+					/>
+					<div className="patient-reg-input">
+						<InputForm
+							type="text"
+							width={"23rem"}
+							length={"1rem"}
+							placeholder={"Username"}
+						/>
+						<InputForm
+							type="password"
+							width={"23rem"}
+							length={"1rem"}
+							placeholder={"Password"}
+						/>
+						<InputForm
+							type="text"
+							width={"23rem"}
+							length={"1rem"}
+							placeholder={"First Name"}
+						/>
+						<InputForm
+							type="text"
+							width={"23rem"}
+							length={"1rem"}
+							placeholder={"Last Name"}
+						/>
+						<InputForm
+							className="email-input"
+							type="email"
+							width={"23rem"}
+							length={"1rem"}
+							placeholder={"Email"}
+						/>
+					</div>
+				</div>
 			</form>
 		</div>
 	);
