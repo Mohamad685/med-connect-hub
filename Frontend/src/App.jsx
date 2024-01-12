af133b4eaffd6b5f1499033d60d8baa754a7d1e1
unknown <mohammad.fakih685@gmail.com>
2024-01-12 11:00:52 +0200

    Frontend<>Added route to doctors page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./Components/Footer/Footer";
 import InputForm from "./Components/Input/Input";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Auth/Auth";
+import PatientRegister from "./Pages/PatientRegister/PatientRegister";
 function App() {
 	return (
 		<BrowserRouter>
@@ -13,7 +14,10 @@ function App() {
 					path="/"
 					element={<HomePage />}
 				/>
-				<Route path="/doctor" element=
+				<Route
+					path="/doctor"
+					element={<PatientRegister />}
+				/>
 			</Routes>
 			<Footer />
 			{/* <Login/> */}
