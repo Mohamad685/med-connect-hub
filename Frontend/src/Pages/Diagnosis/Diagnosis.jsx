6e84945f1299b901c6c1d537c4568aaf67cfb6e4
unknown <mohammad.fakih685@gmail.com>
2024-01-13 14:43:51 +0200

    Frontend<> Added the inputs of diagnosis page

--- Frontend/src/Pages/Diagnosis/Diagnosis.jsx
+++ Frontend/src/Pages/Diagnosis/Diagnosis.jsx
@@ -1,24 +1,52 @@
 import React from "react";
 import OptionsBox from "../../Components/Options/Options";
 import "./Diagnosis.css";
+import ProfilePic from "../../Components/ProfilePic/ProfilePic";
+import TextArea from "../../Components/TextArea/TextArea";
+import Button from "../../Components/Button/Button";
 
+function Diagnosis() {
+	return (
+		<>
+			<div className="patient-reg-page">
+				<OptionsBox margin={"4rem 2rem 2rem 2rem"} />
 
-function Diagnosis(){
-    return(
-    
-    <>
-    <div className="patient-reg-page">
-			<OptionsBox
-				margin={"4rem 2rem 2rem 2rem"}
-			/>
-            
-            
-            
-            
-            
-            </div>
-    
-    </>)
+				<form className="patient-reg-form">
+					<p className="patient-reg-title">Create Patient Profile</p>
+					<div className="patient-reg-section1">
+						<ProfilePic />
+						<div className="patient-form-input">
+							<div className="address-input-div">
+								<TextArea
+									width={"48rem"}
+									length={"8rem"}
+									textAlign={"text-top"}
+									placeholder={"Address"}
+								/>
+								<TextArea
+									width={"48rem"}
+									length={"18rem"}
+									placeholder={"Medical History"}
+								/>
+								<TextArea
+									width={"48rem"}
+									length={"18rem"}
+									placeholder={"Medication History"}
+								/>
+								<Button
+									width={"48rem"}
+									height={"3rem"}
+									color={"white"}
+									fontSize="1.15rem"
+									text={"Submit"}
+								/>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
 
 export default Diagnosis;
