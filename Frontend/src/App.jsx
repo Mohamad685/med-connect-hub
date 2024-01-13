4cf1ab789b7cdb94f671b961a71b454aa3e85abe
unknown <mohammad.fakih685@gmail.com>
2024-01-13 02:25:16 +0200

    Frontend<> Cleaned the app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import NavBar from "./Components/NavBar/NavBar";
 import HomePage from "./Pages/HomePage/Homepage";
 import Footer from "./Components/Footer/Footer";
-import InputForm from "./Components/Input/Input";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Auth/Auth";
 import PatientRegister from "./Pages/PatientRegister/PatientRegister";
-import OptionsBox from "./Components/Options/Options";
-import ProfilePic from "./Components/ProfilePic/ProfilePic";
+
 function App() {
 	return (
 		<BrowserRouter>
