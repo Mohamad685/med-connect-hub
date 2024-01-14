2f3fef985d83552fc95d6b3621811592d8a82fb1
unknown <mohammad.fakih685@gmail.com>
2024-01-14 22:58:01 +0200

    Server<>Added records to PatientsSeeders

--- server/database/seeders/PatientsTableSeeder.php
+++ server/database/seeders/PatientsTableSeeder.php
@@ -11,22 +11,75 @@ use App\Models\Patient;
 class PatientsTableSeeder extends Seeder
 {
     public function run()
-    {
-        $user = User::create([
-            'user_name' => 'ali123',
-            'email' => 'ali@example.com',
-            'password' => Hash::make('password'), 
-            'role' => 'patient'
-        ]);
+{
+    $user1 = User::create([
+        'user_name' => 'ali123',
+        'email' => 'ali@example.com',
+        'password' => Hash::make('password'), 
+        'role' => 'patient'
+    ]);
 
-        Patient::create([
-            'user_id' => $user->id, 
-            'first_name'=>'ali',
-            'last_name'=> 'fakih',
-            'gender'=> 'male',
-            'date_of_birth'=>'1992-10-1',
-            'phone_number'=>'012345678911',
-            'address'=>'beirut'
-        ]);
-    }
+    Patient::create([
+        'user_id' => $user1->id, 
+        'first_name' => 'Ali',
+        'last_name' => 'Fakih',
+        'gender' => 'male',
+        'date_of_birth' => '1992-10-01',
+        'phone_number' => '012345678911',
+        'address' => 'Beirut'
+    ]);
+
+    $user2 = User::create([
+        'user_name' => 'sara456',
+        'email' => 'sara@example.com',
+        'password' => Hash::make('password'), 
+        'role' => 'patient'
+    ]);
+
+    Patient::create([
+        'user_id' => $user2->id, 
+        'first_name' => 'Sara',
+        'last_name' => 'Ahmed',
+        'gender' => 'female',
+        'date_of_birth' => '1990-08-15',
+        'phone_number' => '0987654321',
+        'address' => 'Tripoli'
+    ]);
+
+    $user3 = User::create([
+        'user_name' => 'mike789',
+        'email' => 'mike@example.com',
+        'password' => Hash::make('password'), 
+        'role' => 'patient'
+    ]);
+
+    Patient::create([
+        'user_id' => $user3->id, 
+        'first_name' => 'Mike',
+        'last_name' => 'Ross',
+        'gender' => 'male',
+        'date_of_birth' => '1988-05-20',
+        'phone_number' => '0234567890',
+        'address' => 'Sidon'
+    ]);
+
+    $user4 = User::create([
+        'user_name' => 'lily010',
+        'email' => 'lily@example.com',
+        'password' => Hash::make('password'), 
+        'role' => 'patient'
+    ]);
+
+    Patient::create([
+        'user_id' => $user4->id, 
+        'first_name' => 'Lily',
+        'last_name' => 'Johnson',
+        'gender' => 'female',
+        'date_of_birth' => '1995-12-30',
+        'phone_number' => '0345678901',
+        'address' => 'Jounieh'
+    ]);
+
+}
+
 }
