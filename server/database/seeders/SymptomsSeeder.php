4e97958c9ae8eef7a117017c9b42d5f7a0a792dc
unknown <mohammad.fakih685@gmail.com>
2024-01-15 01:14:52 +0200

    Server<>Implemented the logic of symptoms seeder

--- server/database/seeders/SymptomsSeeder.php
+++ server/database/seeders/SymptomsSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class SymptomsSeeder extends Seeder
 {
     /**
@@ -12,6 +12,25 @@ class SymptomsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $patients = DB::table('patients')->take(4)->get();
+        $doctors = DB::table('doctors')->take(4)->get();
+        $symptom_description = [
+            'Panadol',
+            'Nexium',
+            'Normix',
+            'Augmentin'
+        ];
+
+        foreach ($patients as $index => $patient) {
+            if (isset($doctors[$index]) && isset($symptom_description[$index])) {
+                DB::table('symptoms')->insert([
+                    'patient_id' => $patient->id,
+                    'symptom_description' => $symptom_description[$index],
+                    'doctor_id' => $doctors[$index]->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
