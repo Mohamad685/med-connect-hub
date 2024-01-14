945da771933bf055c955e024befa1bd934232458
unknown <mohammad.fakih685@gmail.com>
2024-01-14 19:46:00 +0200

    Server<>Added getLabResults function in patient controller

--- server/app/Http/Controllers/PatientController.php
+++ server/app/Http/Controllers/PatientController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    //
-}
+    public function getLabResults($patientId){
+        $patient=Patient::find($patientId);
+        if(auth()->user()->id !== $patient -> id){
+            return response()->json(["error"=> 'Unauthorized'],403);
+        }
+        $medicalHistory = $patient->medicalHistories; // Assuming a one-to-many relationship
+        return response()->json($medicalHistory);
+    }
+    }
