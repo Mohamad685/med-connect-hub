1144e355d63805afdc6c724eec3c928df1b08f67
unknown <mohammad.fakih685@gmail.com>
2024-01-15 01:02:17 +0200

    Server<>Implemented the logic of diagnosis seeder

--- server/database/seeders/DiagnosisSeeder.php
+++ server/database/seeders/DiagnosisSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,6 +13,25 @@ class DiagnosisSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $patients = DB::table('patients')->take(4)->get();
+        $doctors = DB::table('doctors')->take(4)->get();
+        $diagnosis_description = [
+            'Sinusitis',
+            'Tonsilitis',
+            'Cancer',
+            'Constipation'
+        ];
+
+        foreach ($patients as $index => $patient) {
+            if (isset($doctors[$index]) && isset($diagnosis_description[$index])) {
+                DB::table('Diagnoses')->insert([
+                    'patient_id' => $patient->id,
+                    'diagnosis_description' => $diagnosis_description[$index],
+                    'doctor_id' => $doctors[$index]->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
--- server/database/seeders/MedicalHistoriesSeeder.php
+++ server/database/seeders/MedicalHistoriesSeeder.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PatientCredentialsSeeder extends Seeder
+class MedicalHistoriesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        // Get the first four patients
         $patients = DB::table('patients')->take(4)->get();
-
-        // Get the first four doctors
         $doctors = DB::table('doctors')->take(4)->get();
-
-        // Descriptions for each medical history
         $descriptions = [
             'General check-up and consultation.',
             'Follow-up for previous condition.',
@@ -28,7 +23,6 @@ class PatientCredentialsSeeder extends Seeder
         ];
 
         foreach ($patients as $index => $patient) {
-            // Ensure there's a corresponding doctor and description for each patient
             if (isset($doctors[$index]) && isset($descriptions[$index])) {
                 DB::table('medical_histories')->insert([
                     'patient_id' => $patient->id,
