810238a95c23954b38c5fe35cbfc4fcdd23ce4d4
unknown <mohammad.fakih685@gmail.com>
2024-01-14 02:39:48 +0200

    Frontend<> Designed Diagnosis page

--- Frontend/src/Pages/Diagnosis/Diagnosis.jsx
+++ Frontend/src/Pages/Diagnosis/Diagnosis.jsx
@@ -1,56 +1,57 @@
 import React from "react";
 import OptionsBox from "../../Components/Options/Options";
-import "./Diagnosis.css";
 import ProfilePic from "../../Components/ProfilePic/ProfilePic";
+import "./Diagnosis.css";
+import PreviewBox from "../../Components/PreviewBox/PreviewBox";
 import TextArea from "../../Components/TextArea/TextArea";
 import Button from "../../Components/Button/Button";
 
 function Diagnosis() {
-	return (
-		<>
-			<div className="patient-reg-page">
-				<OptionsBox margin={"4rem 2rem 2rem 2rem"} />
 
-				<form className="patient-reg-form">
-					<p className="patient-reg-title">Patient Full Name</p>
-					<div className="patient-reg-section1">
-						<ProfilePic />
-						<div className="patient-form-input">
-							<div className="address-input-div">
-								<TextArea
-									width={"48rem"}
-									length={"8rem"}
-									textAlign={"text-top"}
-									placeholder={"Lab Results"}
-								/>
-								<TextArea
-									width={"48rem"}
-									length={"18rem"}
-									placeholder={"Symptoms"}
-								/>
-								<TextArea
-									width={"48rem"}
-									length={"18rem"}
-									placeholder={"Diagnosis"}
-								/>
-                                <TextArea
-									width={"48rem"}
-									length={"18rem"}
-									placeholder={"Prescription"}
-								/>
-								<Button
-									width={"48rem"}
-									height={"3rem"}
-									color={"white"}
-									fontSize="1.15rem"
-									text={"Submit"}
-								/>
-							</div>
-						</div>
+	return (
+		<div className="patient-page">
+			<OptionsBox margin={"4rem 2rem 2rem 2rem"} />
+
+			<div className="patient-data-form">
+				<p className="patient-name">Mohammad Fakih</p>
+				<div className="patient-preview-section1">
+					<div className="patient-preview-boxes">
+						<PreviewBox
+							width={"60rem"}
+							height={"auto"}
+                            title={"Medical History:"}
+                            text={"Hello word"}
+						/>
+						<PreviewBox
+							width={"60rem"}
+							height={"auto"}
+                            title={"Medication History:"}
+                            text={"Hello word"}
+						/>
+						<TextArea
+								width={"60rem"}
+								length={"18rem"}
+								placeholder={"Symptoms:"}
+							/>
+							<TextArea
+								width={"60rem"}
+								length={"18rem"}
+								placeholder={"Lab Results:"}
+							/>
+							<TextArea
+								width={"60rem"}
+								length={"18rem"}
+								placeholder={"Diagnosis:"}
+							/>
+							<TextArea
+								width={"60rem"}
+								length={"18rem"}
+								placeholder={"Prescriptions:"}
+							/>
 					</div>
-				</form>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
