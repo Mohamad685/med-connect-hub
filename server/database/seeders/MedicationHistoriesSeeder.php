ae89600e0d3d9b9150122144d3b6d869aab329d0
unknown <mohammad.fakih685@gmail.com>
2024-01-15 01:13:24 +0200

    Server<>Implemented the logic of prescriptions seeder

--- server/database/seeders/MedicationHistoriesSeeder.php
+++ server/database/seeders/MedicationHistoriesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -31,5 +32,6 @@ class MedicationHistoriesSeeder extends Seeder
                     'updated_at' => now(),
                 ]);
             }
-        }    }
+        }
+    }
 }
--- server/database/seeders/PrescriptionsSeeder.php
+++ server/database/seeders/PrescriptionsSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class PrescriptionsSeeder extends Seeder
 {
     /**
@@ -12,6 +12,25 @@ class PrescriptionsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $patients = DB::table('patients')->take(4)->get();
+        $doctors = DB::table('doctors')->take(4)->get();
+        $medication_description = [
+            'Panadol',
+            'Nexium',
+            'Normix',
+            'Augmentin'
+        ];
+
+        foreach ($patients as $index => $patient) {
+            if (isset($doctors[$index]) && isset($medication_description[$index])) {
+                DB::table('prescriptions')->insert([
+                    'patient_id' => $patient->id,
+                    'medication_description' => $medication_description[$index],
+                    'doctor_id' => $doctors[$index]->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
