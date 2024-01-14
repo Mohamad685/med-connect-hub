935d6c495a8aa059410ffbc452e0b2c7cb19ad0d
unknown <mohammad.fakih685@gmail.com>
2024-01-15 01:05:55 +0200

    Server<>Implemented the logic of LabResults seeder

--- server/database/seeders/LabResultsSeeder.php
+++ server/database/seeders/LabResultsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +12,24 @@ class LabResultsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-    }
+        $patients = DB::table('patients')->take(4)->get();
+        $doctors = DB::table('doctors')->take(4)->get();
+        $result = [
+            'Glucose:124',
+            'LDL:120',
+            'HDL:121',
+            'Cholesterol:200'
+        ];
+
+        foreach ($patients as $index => $patient) {
+            if (isset($doctors[$index]) && isset($result[$index])) {
+                DB::table('lab_results')->insert([
+                    'patient_id' => $patient->id,
+                    'result' => $result[$index],
+                    'doctor_id' => $doctors[$index]->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }    }
 }
