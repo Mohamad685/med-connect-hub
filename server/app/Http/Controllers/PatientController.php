bcb1391388a7c3d0757041082ebfd45509f7206e
unknown <mohammad.fakih685@gmail.com>
2024-01-15 14:49:26 +0200

    Server<>Added get prescriptions function

--- server/app/Http/Controllers/PatientController.php
+++ server/app/Http/Controllers/PatientController.php
@@ -107,6 +107,26 @@ public function getSymptoms($user_name)
     $symptoms= $patient->Symptoms; 
     return response()->json($symptoms);
 }
+
+public function getPrescription($user_name)
+{
+    $user =User::where('user_name', $user_name)->first(); 
+    if (!$user) {
+        return response()->json(["error" => "User not found"], 404);
+    }
+    $patient = $user->patient;
+
+    if (!$patient) {
+        return response()->json(["error" => "Patient not found"], 404);
+    }
+
+    if (auth()->user()->id !== $user->id) {
+        return response()->json(["error" => 'Unauthorized'], 403);
+    }
+
+    $prescription= $patient->prescriptions; 
+    return response()->json($prescription);
+}
     
 }
 
