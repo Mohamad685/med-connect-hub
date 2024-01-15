874995e9d85a45de76fd0eab5f9de8e84b8fee79
unknown <mohammad.fakih685@gmail.com>
2024-01-15 21:36:10 +0200

    Server<> Added medical record model relations

--- server/app/Models/MedicalRecord.php
+++ server/app/Models/MedicalRecord.php
@@ -14,10 +14,41 @@ class MedicalRecord extends Model
         'medicalHistory_id',
         'medicationHistory_id',
         'labResults_id',
-        'symptom_i',
+        'symptom_id',
         'prescription_id',
         'diagnosis_id',
     ];    
 
-    
+    public function doctor(){
+        return $this->belongsToMany(Doctor::class,'doctor_id');
+    }
+
+    public function patient(){
+        return $this->belongsToMany(Patient::class,'patient_id');
+    }
+
+
+    public function medicalHistory(){
+        return $this->belongsToMany(medicalHistory::class,'medicalHistory_id');
+    }
+
+    public function medicationHistory(){
+        return $this->belongsToMany(MedicalHistory::class,'medicationHistory_id');
+    }
+
+    public function labResults(){
+        return $this->belongsToMany(LabResult::class,'labResults_id');
+    }
+
+    public function symptom(){
+        return $this->belongsToMany(Symptom::class,'symptom_id');
+    }
+
+    public function prescription(){
+        return $this->belongsToMany(Prescription::class,'prescription_id');
+    }
+
+    public function diagnosis(){
+        return $this->belongsToMany(Diagnosis::class,'diagnosis_id');
+    }
 }
