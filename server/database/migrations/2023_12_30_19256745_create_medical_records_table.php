9000d04d1c0525913cfbd1e3f5ec76eba6cdcaeb
unknown <mohammad.fakih685@gmail.com>
2024-01-15 18:57:35 +0200

    Server<> Added the function to medical records controller

--- server/database/migrations/2023_12_30_19256745_create_medical_records_table.php
+++ server/database/migrations/2023_12_30_19256745_create_medical_records_table.php
@@ -17,15 +17,18 @@ return new class extends Migration
             $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
             $table->unsignedBigInteger('patient_id');
             $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
-            // $table->unsignedBigInteger('patient_id');
-            // $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
-            // $table->unsignedBigInteger('patient_id');
-            // $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
-            // $table->unsignedBigInteger('patient_id');
-            // $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
-            // $table->unsignedBigInteger('patient_id');
-            // $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
-            
+            $table->unsignedBigInteger('medicalHistory_id');
+            $table->foreign('medicalHistory_id')->references('id')->on('medical_histories')->onDelete('cascade');
+            $table->unsignedBigInteger('medicationHistory_id');
+            $table->foreign('medicationHistory_id')->references('id')->on('medication_histories')->onDelete('cascade');
+            $table->unsignedBigInteger('labResults_id');
+            $table->foreign('labResults_id')->references('id')->on('lab_results')->onDelete('cascade');
+            $table->unsignedBigInteger('symptom_id');
+            $table->foreign('symptom_id')->references('id')->on('symptoms')->onDelete('cascade');
+            $table->unsignedBigInteger('prescription_id');
+            $table->foreign('prescription_id')->references('id')->on('prescriptions')->onDelete('cascade');
+            $table->unsignedBigInteger('diagnosis_id');
+            $table->foreign('diagnosis_id')->references('id')->on('diagnoses')->onDelete('cascade');
             $table->timestamps();
         });
     }
