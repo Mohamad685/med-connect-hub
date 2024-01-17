5ebf95abea3004ceaa5a75a63e28045ca812ed75
unknown <mohammad.fakih685@gmail.com>
2024-01-17 10:53:59 +0200

    Frontend<> Auth fetched with backend

--- Frontend/src/Components/Auth/Auth.jsx
+++ Frontend/src/Components/Auth/Auth.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 import "./Auth.css";
 import InputForm from "../Input/Input";
 import Button from "../Button/Button";
 
-function Login({onClose}) {
+function Login({ onClose }) {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [emailError, setEmailError] = useState("");
 	const [passwordError, setPasswordError] = useState("");
-
+	const history = useHistory();
 	const handleClose = () => {
-        onClose();
-    };
-	
+		onClose();
+	};
+
 	const handleEmailChange = (e) => {
 		setEmail(e.target.value);
 		setEmailError("");
@@ -43,48 +45,67 @@ function Login({onClose}) {
 		}
 
 		if (valid) {
-			setEmail("");
-			setPassword("");
+			axios
+				.post("http://localhost:8000/login", { email, password })
+				.then((response) => {
+					if (response.data.user.role === "admin") history.push("/admin");
+					else if (response.data.user.role === "doctor")
+						history.push("/patient-registration");
+					else if (response.data.user.role === "patient")
+						history.push("/patient-file");
+					else if (response.data.user.role === "insuracne")
+						history.push("/insurance-page");
+				})
+				.catch((error) => {
+					console.error(error);
+				});
 		}
+
+		setEmail("");
+		setPassword("");
 	};
-
-	return (
-		<>
-			<form
-				className="login-form"
-				onSubmit={handleSubmit}>
-				<button onClick={handleClose} className="close-button">X</button>
-				<h1>Log In</h1>
-				<InputForm
-					type="email"
-					width={"25rem"}
-					length={"2.5rem"}
-					placeholder={"Enter your email:"}
-					value={email}
-					onChange={handleEmailChange}
-				/>
-				{emailError && <div className="error">{emailError}</div>}
-
-				<InputForm
-					type="password"
-					width={"25rem"}
-					length={"2.5rem"}
-					placeholder={"Enter your password:"}
-					value={password}
-					onChange={handlePasswordChange}
-				/>
-				{passwordError && <div className="error">{passwordError}</div>}
-
-				<Button
-					color={"white"}
-					width={"14rem"}
-					height={"2.5rem"}
-					text={"Login"}
-					classNames={["button-style"]}
-				/>
-			</form>
-		</>
-	);
 }
+
+return (
+	<>
+		<form
+			className="login-form"
+			onSubmit={handleSubmit}>
+			<button
+				onClick={handleClose}
+				className="close-button">
+				X
+			</button>
+			<h1>Log In</h1>
+			<InputForm
+				type="email"
+				width={"25rem"}
+				length={"2.5rem"}
+				placeholder={"Enter your email:"}
+				value={email}
+				onChange={handleEmailChange}
+			/>
+			{emailError && <div className="error">{emailError}</div>}
+
+			<InputForm
+				type="password"
+				width={"25rem"}
+				length={"2.5rem"}
+				placeholder={"Enter your password:"}
+				value={password}
+				onChange={handlePasswordChange}
+			/>
+			{passwordError && <div className="error">{passwordError}</div>}
+
+			<Button
+				color={"white"}
+				width={"14rem"}
+				height={"2.5rem"}
+				text={"Login"}
+				classNames={["button-style"]}
+			/>
+		</form>
+	</>
+);
 
 export default Login;
