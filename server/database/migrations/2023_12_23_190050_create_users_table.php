eeac957a8aa4f22052ca5e460efabc2cf7c94ef1
unknown <mohammad.fakih685@gmail.com>
2024-01-18 15:56:06 +0200

    Server<> Added profile pic to the users table

--- server/database/migrations/2023_12_23_190050_create_users_table.php
+++ server/database/migrations/2023_12_23_190050_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role'); //doctor, patient, insurance
+            $table->string('profile_picture')->nullable();
+
             // $table->string('userable_type')->nullable();
             // $table->unsignedBigInteger('userable_id')->nullable();
 
