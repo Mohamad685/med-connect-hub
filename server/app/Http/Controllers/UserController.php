f8b10bc2f273ddaabce1c1e988522cf841911cdc
unknown <mohammad.fakih685@gmail.com>
2024-01-18 17:32:09 +0200

    Server<> Added usercontroller logic

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\RegistrationService;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    //
+    protected $registrationService;
+
+    public function __construct(RegistrationService $registrationService)
+    {
+        $this->registrationService = $registrationService;
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $user = $this->registrationService->registerUser($request->all());
+            return response()->json(['message' => 'User registered successfully!', 'profile_picture' => $user->profile_picture], 200);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->validator->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
 }
