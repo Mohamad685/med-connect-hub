61f40563d26ebfdd37e754762c958b049804fba0
unknown <mohammad.fakih685@gmail.com>
2024-01-19 00:01:42 +0200

    Server<> Added PatientRegistration Service

---
+++ server/app/Services/PatientRegistrationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Doctor;
+use App\Models\MedicationHistory;
+use App\Models\MedicalHistory;
+use Illuminate\Support\Facades\Auth;
+
+class PatientRegistrationService
+{
+    public function handleRegistration($data)
+    {
+        
+    }
+
+}
