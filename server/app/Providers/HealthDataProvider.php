926c6a4e27213f3687e032b8e7b7d58bbbf17d5a
unknown <mohammad.fakih685@gmail.com>
2024-01-20 00:01:11 +0200

    Server<> Added healthdata provider file

---
+++ server/app/Providers/HealthDataProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HealthDataProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
