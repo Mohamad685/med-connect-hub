ef1f44dcd5238dcfdf4785f65bdd13a5e19c2049
unknown <mohammad.fakih685@gmail.com>
2024-01-20 18:18:52 +0200

    Frontend<> Fetched the patient file page

--- Frontend/src/Pages/PatientPage/PatientPage.jsx
+++ Frontend/src/Pages/PatientPage/PatientPage.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OptionsBox from "../../Components/Options/Options";
 import ProfilePic from "../../Components/ProfilePic/ProfilePic";
 import "./PatientPage.css";
 import PreviewBox from "../../Components/PreviewBox/PreviewBox";
+import fetchHelper from "../../Components/Functions/FetchFunction";
 
 function PatientPreview() {
+	const [labResults, setLabResults] = useState("");
+	const [symptoms, setSymptoms] = useState("");
+	const [diagnosis, setDiagnosis] = useState("");
+	const [prescriptions, setPrescriptions] = useState("");
+	useEffect(() => {
+		const patientId = localStorage.getItem("patientId");
+		const fetchData = async () => {
+			try {
+				const labResultsData = await fetchHelper.get(
+					`/patient/${patientId}/lab-results`
+				);
+				console.log('results:',labResultsData)
+				setLabResults(labResultsData);
 
+				const symptomsData = await fetchHelper.get(
+					`/patient/${patientId}/symptoms`
+				);
+				setSymptoms(symptomsData);
+
+				const diagnosisData = await fetchHelper.get(
+					`/patient/${patientId}/diagnosis`
+				);
+				setDiagnosis(diagnosisData);
+
+				const prescriptionsData = await fetchHelper.get(
+					`/patient/${patientId}/prescriptions`
+				);
+				setPrescriptions(prescriptionsData);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				console.log(error.response)
+			}
+		};
+
+		fetchData();
+	}, []);
 	return (
 		<div className="patient-page">
 			<OptionsBox margin={"8rem 2rem 2rem 2rem"} />
@@ -18,26 +54,26 @@ function PatientPreview() {
 						<PreviewBox
 							width={"48rem"}
 							height={"auto"}
-                            title={"Lab Results:"}
-                            text={"Hello word"}
+							title={"Lab Results:"}
+							text={labResults}
 						/>
 						<PreviewBox
 							width={"48rem"}
 							height={"auto"}
-                            title={"Symptoms:"}
-                            text={"Hello word"}
+							title={"Symptoms:"}
+							text={symptoms}
 						/>
-                        <PreviewBox
+						<PreviewBox
 							width={"48rem"}
 							height={"auto"}
-                            title={"Diagnosis:"}
-                            text={"Hello word"}
+							title={"Diagnosis:"}
+							text={diagnosis}
 						/>
-                        <PreviewBox
+						<PreviewBox
 							width={"48rem"}
 							height={"auto"}
-                            title={"Prescription:"}
-                            text={"Hello word"}
+							title={"Prescription:"}
+							text={prescriptions}
 						/>
 					</div>
 				</div>
