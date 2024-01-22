a3256b47d92d14320547d383e7a21844a26b0c97
unknown <mohammad.fakih685@gmail.com>
2024-01-23 01:49:05 +0200

    Frontend <>Updated the profile pic component

--- Frontend/src/Components/ProfilePic/ProfilePic.jsx
+++ Frontend/src/Components/ProfilePic/ProfilePic.jsx
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from "react";
-import axios from "axios";
-import Button from "../Button/Button";
 import "./ProfilePic.css";
-const ProfilePic = () => {
+const ProfilePic = ({ onFileSelect }) => {
 	const [preview, setPreview] = useState(null);
 	const fileInputRef = useRef(null);
 
@@ -10,6 +8,7 @@ const ProfilePic = () => {
 		const file = e.target.files[0];
 		if (file && file.type.startsWith("image/")) {
 			setPreview(URL.createObjectURL(file));
+			onFileSelect(file);
 		}
 	};
 
@@ -17,48 +16,24 @@ const ProfilePic = () => {
 		fileInputRef.current.click();
 	};
 
-	const handleUpload = async () => {
-		const file = fileInputRef.current.files[0];
-		if (!file) return;
-
-		const formData = new FormData();
-		formData.append("image", file);
-
-		try {
-			const response = await axios.post("/upload", formData, {
-				headers: {
-					"Content-Type": "multipart/form-data",
-				},
-			});
-		} catch (error) {}
-	};
-
 	return (
-		<div className="profile-pic-form">
-			<div
-				onClick={handleBoxClick}
-				className="pic-box">
-				{!preview && <span>Add Profile Pic</span>}
-				{preview && (
-					<img
-						src={preview}
-						alt="Preview"
-						className="profile-pic"
-					/>
-				)}
-				<input
-					type="file"
-					style={{ display: "none" }}
-					onChange={handleFileChange}
-					ref={fileInputRef}
+		<div
+			onClick={handleBoxClick}
+			className="pic-box">
+			{!preview && <span>Add Profile Pic</span>}
+			{preview && (
+				<img
+					src={preview}
+					alt="Preview"
+					className="profile-pic"
 				/>
-			</div>
-			{preview && <Button
-								width={"10rem"}
-								height={"1.5rem"}
-								color={"white"}
-								fontSize='0.8rem'
-								text={'Upload Pic'}onClick={handleUpload}>Upload Image</Button>}
+			)}
+			<input
+				type="file"
+				style={{ display: "none" }}
+				onChange={handleFileChange}
+				ref={fileInputRef}
+			/>
 		</div>
 	);
 };
