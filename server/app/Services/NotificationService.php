e1d2f280abdc69f46988f92e9bf4b1cc426f97d0
unknown <mohammad.fakih685@gmail.com>
2024-01-25 12:09:31 +0200

    Server<>Updated the insurance approval service

--- server/app/Services/NotificationService.php
+++ server/app/Services/NotificationService.php
@@ -2,14 +2,11 @@
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Laravel\Firebase\Facades\Firebase;
 
-
-
-
 class NotificationService{
     public function sendTo($title,$token,$body){
         $messaging=Firebase::messaging();
         $message=CloudMessage::withTarget('token',$token)->withNotification(Notification::create($title,$body));
-        $messaging=send($message);
+        $messaging->send($message);
 
     }
 }
